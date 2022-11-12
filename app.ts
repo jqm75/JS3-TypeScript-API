847fc5675989c909e140dd8a3b471e2bed7dd2d4
Quim <joaquimpujol@gmail.com>
2022-11-12 16:38:15 +0100

    Add comments

--- app.ts
+++ app.ts
@@ -1,16 +1,20 @@
+// Creamos función asincrona para esperar la promesa.
 async function callRandomJoke() {
+    // Almacenamos la URL de la API.
     const API_URL = 'https://icanhazdadjoke.com/';
-    
+
+    // Añadimos el HEADER que especifica la API en su documentación.
     const options = {
         headers: {
             'Accept': 'application/json'
         }
     };
-
+// Hacemos la petición/fetch a la API y lo convertimos a JSON.
     const jokeResponse = await (await fetch(API_URL, options)).json()
-    console.log("🚀 ~ file: app.ts ~ line 11 ~ callRandomJoke ~ joke", joke)
-    
+    console.log("🚀 ~ file: app.ts ~ line 14 ~ callRandomJoke ~ jokeResponse", jokeResponse)
+        
     const HTMLResponse = document.querySelector('#joke')
 
+    // Imprimimos respuesta especificando el atributo del joke (+info en console.log línea 14)
     HTMLResponse.innerHTML = jokeResponse.joke;
 }
--- build/app.js
+++ build/app.js
@@ -8,17 +8,22 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+// Creamos función asincrona para esperar la promesa.
 function callRandomJoke() {
     return __awaiter(this, void 0, void 0, function* () {
+        // Almacenamos la URL de la API.
         const API_URL = 'https://icanhazdadjoke.com/';
+        // Añadimos el HEADER que especifica la API en su documentación.
         const options = {
             headers: {
                 'Accept': 'application/json'
             }
         };
+        // Hacemos la petición/fetch a la API y lo convertimos a JSON.
         const jokeResponse = yield (yield fetch(API_URL, options)).json();
-        console.log("🚀 ~ file: app.ts ~ line 11 ~ callRandomJoke ~ joke", joke);
+        console.log("🚀 ~ file: app.ts ~ line 14 ~ callRandomJoke ~ jokeResponse", jokeResponse);
         const HTMLResponse = document.querySelector('#joke');
+        // Imprimimos respuesta especificando el atributo del joke (+info en console.log línea 14)
         HTMLResponse.innerHTML = jokeResponse.joke;
     });
 }
